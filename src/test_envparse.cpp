e1768c57376a85f310a71b6b02314e94170c2d40
drkwitht <derektan3510@gmail.com>
2025-01-18 05:49:43 -0800

    Finalize setup.

---
+++ src/test_envparse.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file test_envparse.cpp
+ * @author DrkWithT
+ * @brief Implements test program for my envfile parser.
+ * @date 2025-01-18
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+int main() {
+    ;
+}
