3dfa20f91655fe9c7d192acca840008825a8c3e0
drkwitht <derektan3510@gmail.com>
2025-01-18 08:07:21 -0800

    Added test, todo.

--- src/test_envparse.cpp
+++ src/test_envparse.cpp
@@ -8,6 +8,31 @@
  * 
  */
 
-int main() {
-    ;
+#include <format>
+#include <iostream>
+#include "api/myenv.hpp"
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << std::format("usage: {} <case-number> <file-path>\n", argv[0]);
+        return 1;
+    }
+
+    auto case_num = std::stoi(argv[1]);
+
+    if (case_num == 3) {
+        auto doc = CPPEnvFile::Api::parseEnv(argv[2]);
+
+        if (not doc.hasEntry("foo")) {
+            return 1;
+        }
+
+        if (not doc.hasEntry("bar")) {
+            return 1;
+        }
+
+        if (auto baz_value = std::get<std::string>(doc.viewEntry("baz").data); baz_value != "123") {
+            return 1;
+        }
+    }
 }
